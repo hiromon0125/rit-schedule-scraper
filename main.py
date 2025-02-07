90cfd78dcb760705ff2215ef94a3972402975d0c
Hiro <hiroto125takeuchi@gmail.com>
2025-02-07 17:23:43 -0500

    feat: argumented path to data directory

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-import sys
+import argparse
+from dataclasses import dataclass
+from pathlib import Path
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+
+
+@dataclass(frozen=True)
+class ScraperArguments:
+    links: list[str]
+    data_path: str = "data"  # Default value
+
+
+def get_arguments() -> ScraperArguments:
+    parser = argparse.ArgumentParser(
+        description="Process a named argument and multiple links."
+    )
+
+    # Define the parameterized argument (e.g., --name value)
+    parser.add_argument(
+        "--data_path", help="An optional named parameter.", default="./data"
+    )
+
+    # Define positional arguments for links
+    parser.add_argument("links", nargs="*", help="List of links.")
+
+    args = parser.parse_args()
+    return ScraperArguments(args.links, args.data_path)
 
 
 def parse(url: str):
@@ -38,18 +63,23 @@ def df_to_csv(df, filename):
     df.to_csv(filename, index=False)
 
 
-def main(*links):
-    for link in links:
+def main():
+    args = get_arguments()
+
+    # Create data directory if it does not exist
+    Path(args.data_path).mkdir(parents=True, exist_ok=True)
+
+    for link in args.links:
         t = parse(link)
         if not t:
             print(f"Failed {link}")
             continue
         df = table_to_df(t)
-        new_filename = f"{link.split('/')[-1]}.csv"
+        new_filename = Path(args.data_path, f"{link.split('/')[-1]}.csv")
         df_to_csv(df, new_filename)
         print(f"Success {link}\n{new_filename}")
     print("All done")
 
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    main()
